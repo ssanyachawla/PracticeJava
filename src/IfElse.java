17add6c0570cb6e9be9e8694cbb8196a67b0a6fe
Sanya <chawlasanya16@gmail.com>
2025-01-16 11:04:31 +0530

    First Commit

---
+++ src/IfElse.java
@@ -0,0 +1,10 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int x = 50;
+        int y = 28;
+
+        if (x > y) {
+            System.out.println("x is greater than y");
+        }
+    }
+}
