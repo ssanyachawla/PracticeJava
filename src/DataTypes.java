79e008a94b35bb88ca3508e4d4464bdb856c30f8
Sanya <chawlasanya16@gmail.com>
2025-01-16 18:41:49 +0530

    second commit

---
+++ src/DataTypes.java
@@ -0,0 +1,15 @@
+public class DataTypes {
+    public static void main(String[] args){
+        int age=25;           //Integer variable
+        double height=5.9;    //Double variable
+        String name= "John";  //String variable
+        char grade='A';       //Character variable
+
+        //Print Variables
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+
+    }
+}
