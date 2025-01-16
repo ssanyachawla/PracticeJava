5883a9e047429175d5e25d04c5d479320b54938b
Sanya <chawlasanya16@gmail.com>
2025-01-17 03:23:00 +0530

    commit2

---
+++ src/Person.java
@@ -0,0 +1,23 @@
+class PersonDetails {
+    String name;
+    int age;
+
+    //Constructor
+    PersonDetails(String name, int age){
+        this.name=name;
+        this.age =age;
+    }
+
+    void introduce(){
+        System.out.println("Hi, I am "+ name+ " and I am "+ age + " years old.");
+    }
+}
+public class Person{
+    public static void main(String[] args){
+        PersonDetails person1 = new PersonDetails("Alice", 25);
+        PersonDetails person2 = new PersonDetails("Bob", 30);
+
+        person1.introduce();
+        person2.introduce();
+    }
+}
---
+++ src/PrimeNumbers.java
@@ -0,0 +1,24 @@
+public class PrimeNumbers {
+    public static void main(String[] args){
+        int limit=100;
+        System.out.println("Prime numbers from 1 to "+ limit +"are:");
+
+        //loop through numbers from 2 to the limit
+        for(int num=2;num<=limit;num++){
+            if(isPrime(num)){
+                System.out.println(num);
+            }
+        }
+    }
+    public static boolean isPrime(int number){
+        if (number<=1){
+            return false;
+        }
+        for(int i=2;i<=Math.sqrt(number);i++){
+            if(number % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
