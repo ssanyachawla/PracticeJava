463dc0de0a6a3b4eb2a2c78415e4959a78a03787
Sanya <chawlasanya16@gmail.com>
2025-01-16 20:21:56 +0530

    commit 5

--- src/ObjectExample.java
+++ src/ObjectExample.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+class Car{
+    //Instance variable
+    String make;
+    String model;
+    int year;
 
-public class Object {
+    //Constructor (used to intialize objects)
+    public Car(String make, String model,int year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public void displayInfo(){
+        System.out.println("Car Make:" + make);
+        System.out.println("Car Model:" + model);
+        System.out.println("Car Year:" + year);
+    }
+}
+
+public class ObjectExample {
+    public static void main(String[] args){
+        Car myCar= new Car("Toyato","Corolla",2020);
+        myCar.displayInfo();
+        Car dreamCar = new Car("Mercedes","AMG",2023);
+        dreamCar.displayInfo();
+
+    }
+
 }
